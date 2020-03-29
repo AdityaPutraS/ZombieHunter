5672fedcb973142322bdab620894278248ea7954
AdityaPutraS <13517013@std.stei.itb.ac.id>
2020-03-29 13:10:54 +0700

    Fix tiling & parallax for background

---
+++ TugasPBD/Assets/Script/BGParallaxTiling.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3681456ece9442f4c737badcb143ffe68e8e4539d2352ead2f2737cd28f19f0d
+size 3690
---
+++ TugasPBD/Assets/Script/BackgroundTiling.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c47d34cf4e06aed41cd17f916966ebd7bcff19e4a92ec9ca7103d5c0c967eeb8
+size 2683
