fd0947c85240e24d50520a8424385820eee2330e
AdityaPutraS <13517013@std.stei.itb.ac.id>
2020-03-30 21:31:05 +0800

    Add HealthBar

---
+++ TugasPBD/Assets/Script/Player/Health.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:cc751955c82bf89b41cbb2dd5cc557ab62bd197dc2827b7fb7db6d3875bc0793
+size 1972
---
+++ TugasPBD/Assets/Script/Player/Shooting.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:90e05b703963fc813954da85cd75c0cf52af0e742a5ed1de2312eee4d6ff5d0f
+size 313
