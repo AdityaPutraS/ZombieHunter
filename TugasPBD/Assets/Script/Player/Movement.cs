150457866bd5f96e0f491dadab09258884243bab
AdityaPutraS <13517013@std.stei.itb.ac.id>
2020-03-30 20:12:06 +0800

    Add enemy movement

--- TugasPBD/Assets/Script/Player/Movement.cs
+++ TugasPBD/Assets/Script/Player/Movement.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:26ded3ed99ec40973162ed75908928f9e564a30a24f574b4edafe7f1a85cf78e
-size 1776
+oid sha256:4a1e9dd8d4f3fd63717c7471a71de806eadd05c506650ef13578dd8d608bec1a
+size 1713
--- TugasPBD/Assets/Script/WorldGen.cs
+++ TugasPBD/Assets/Script/WorldGen.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:191a8f5d7e3f6d82b3a674c278335d60756e01e28b48db7a44f31849c4b95baa
-size 3695
+oid sha256:b358abf9f1521066281474c57864bce841401a8341ca32d35ecd3e8dd87a984e
+size 3653
