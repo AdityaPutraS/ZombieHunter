70b598baa9f9cf1dc2e04c8adc0757ab3d0fe061
AdityaPutraS <13517013@std.stei.itb.ac.id>
2020-03-31 10:10:34 +0800

    Add main menu

---
+++ TugasPBD/Assets/Script/Menu/Button.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:fe6eacd3f430a3b8ecc8d358c029b07709809d9eef07bcef21f274d244f2db64
+size 575
---
+++ TugasPBD/Assets/Script/Menu/MovingCloud.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d3e3ea377e6b11647b1ebc75ee002799cb9f1197b7471aecb3a438691c173761
+size 1479
